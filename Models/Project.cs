7b793cfb3ea99da673fb70389a197632f8ce13e2
Matthew Krenner <matthew.krenner@gmail.com>
2022-08-12 18:23:09 +0200

    Create Project model

---
+++ Models/Project.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IssueTracker.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+
+        [StringLength(100), MinLength(3)]
+        [Required]
+        public string Title { get; set; } = default!;
+
+        [StringLength(500)]
+        public string Description { get; set; } = default!;
+
+        [DataType(DataType.Date)]
+        public DateTime Created { get; set; }
+
+    }
+}
