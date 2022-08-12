6620f560bf9750dcfef5ce4e4ac2bccdde4b805c
Matthew Krenner <matthew.krenner@gmail.com>
2022-08-12 18:54:04 +0200

    Create Login Form

---
+++ Pages/Account/Login.cshtml.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace IssueTracker.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public Credential Credential { get; set; } = default!;
+
+        public void OnPost()
+        {
+        }
+    }
+
+    public class Credential
+    {
+        [Required]
+        [Display(Name = "Username")]
+        public string UserName { get; set; } = default!;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = default!;
+    }
+}
